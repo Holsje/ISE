09fc346c62459d986067caacd5f6a864aaa781f7
nielsbrg <nielsbergervoet@gmail.com>
2016-04-18 12:57:42 +0200

    Eerste opzet

---
+++ Congresapplicatie/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Congresapplicatie</title>
+		<?php include 'includes.php'; ?>
+	</head>
+
+	<body>
+		<?php include 'header.php'; ?>
+		<div class="container col-xs-12 col-sm-12 col-md-12">
+			
+		</div>
+	</body>
+</html>
---
+++ Congresapplicatie/menu.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="css/menu/side_menu.css">
+<div id="wrapper">
+	<!-- Sidebar -->
+	<nav class="navbar navbar-inverse" id="sidebar-wrapper" role="navigation">
+		<ul class="nav sidebar-nav">
+			<li class="sidebar-brand"> <a href="/viewposts"> Puk point </a> </li>
+			<li> <a href="/viewposts"><i class="fa fa-fw fa-home"></i> Tijdlijn </a> </li>
+			<li> <a href="/createpost"><i class="fa fa-fw fa-home"></i> Post plaatsen </a> </li>
+				<li class="dropdown"> <a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-fw fa-plus"></i> Beheer <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li class="dropdown-header">Beheer</li>
+						
+					</ul>
+				</li>
+		</ul>
+	</nav>
+	<!-- /#sidebar-wrapper -->
+	<nav class="navbar navbar-inverse navbar-fixed-top" id="topbar-wrapper">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			</div>
+		</div>
+	</nav>
+</div>
+<!-- /#wrapper -->
+<script src="css/menu/slide_menu.js"></script>
