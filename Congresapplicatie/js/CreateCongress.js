211e9359609207f82c776934a8bee8eb39b68962
Onno Hols <Onno Hols>
2016-04-20 14:36:08 +0200

    Ajax & error functie toegevoegd

--- Congresapplicatie/js/CreateCongress.js
+++ Congresapplicatie/js/CreateCongress.js
@@ -13,19 +13,26 @@ function validateForm() {
 		subject == null || subject == "" || startDate == null || startDate == "" || 
 		endDate == null || endDate == "") {
 			
-		alert("Not all fields are filled in.");
+		makeError("Not all fields are filled in.");
 		return false;
 	}
 	
 	if(!isValidDate(startDate) || !isValidDate(endDate)) {
-		alert("Invalid start or end date.");
+		makeError("Invalid start or end date.");
 		return false;
 	}
 	
 	var d1 = new Date(startDate);
 	var d2 = new Date(endDate);
 	if(d1 > d2) {
-		alert("Start date is before end date.");
+		makeError("Start date is before end date.");
 		return false;
 	}
+	
 }
+
+var onFilledForm = function(output) {
+	console.log(output);
+}
+
+
--- Congresapplicatie/js/globalFunctions.js
+++ Congresapplicatie/js/globalFunctions.js
@@ -12,13 +12,40 @@
 		document.body.appendChild(dialog);
 	}
 	else {
-		$("#dialog").title = title;
-		$("#dialogMessage").innerHTML = message;
+		$("#dialog")
+		$(".ui-dialog-title").text(title);
+		$("#dialogMessage").text(message);
 		
 	}
-	
 	
 	$(function() {
 		$( "#dialog" ).dialog();
 	  });
  }
+ 
+ 
+ function getAjax(url,jsonData,handleData) {
+	$.ajax({
+		url: url, 
+		type: 'GET',
+		data : jsonData,
+		contentType: 'application/json; charset=utf-8',
+		success:function(data) {
+			handleData(data)
+		},
+		error: function() {
+			handleData('error')
+		}
+	});	
+}
+
+
+function makeError(errorMessage,id) {
+	if(id) {	
+		$("#" + id).text("* " + errorMessage);
+		$("#" + id)[0].style.display = "";
+	}else {
+		$("#errorMsg").text("* " + errorMessage);
+		$("#errorMsg")[0].style.display = "";
+	}
+}
