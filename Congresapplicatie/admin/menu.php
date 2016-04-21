3998c14019eb98880c9c02b1d14015785c2c786e
ErikEvers <erikevers1996@gmail.com>
2016-04-21 10:08:24 +0200

    Lay-out management application and pop-ups

---
+++ Congresapplicatie/admin/menu.php
@@ -0,0 +1,33 @@
+<nav role="navigation" class="col-xs-12 col-sm-12 col-md-12 navbar navbar-default mobileMenu">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header col-xs-12">
+        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <!-- Collection of nav links and other content for toggling -->
+    <div id="navbarCollapse" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li><a href="#">Congres</a></li>
+            <li><a href="#">Gebouwen</a></li>
+            <li><a href="#">Evenementen</a></li>
+            <li><a href="#">Reviewboard</a></li>
+            <li><a href="#">Medewerkers beheren</a></li>
+        </ul>
+    </div>
+</nav>
+
+<nav role="navigation" class="navbar navbar-default largeMenu col-xs-12 col-sm-12 col-md-12">
+    <div class="navbar-header col-xs-12 col-sm-12 col-md-12">
+        <ul class="nav navbar-nav col-sm-12 col-md-12">
+            <li class="col-sm-2 col-md-2"><a href="#">Congres</a></li>
+            <li class="col-sm-2 col-md-2"><a href="#">Gebouwen</a></li>
+            <li class="col-sm-2 col-md-2"><a href="#">Evenementen</a></li>
+            <li class="col-sm-2 col-md-2"><a href="#">Reviewboard</a></li>
+            <li class="col-sm-2 col-md-2"><a href="#">Medewerkers beheren</a></li>
+        </ul>
+    </div>
+</nav>
--- Congresapplicatie/pageConfig.php
+++ Congresapplicatie/pageConfig.php
@@ -23,7 +23,7 @@
 		<body>
 			<header>
 				<div class="header1 col-xs-12 col-sm-12 col-md-12">
-					<img class="img-responsive logo col-xs-12 col-sm-12 col-md-12" 
+					<img class="img-responsive logo col-xs-12 col-sm-12 col-md-12"
 					src="img/logo template.png" alt="logo">
 				</div>
 					<?php include 'menu.php'; ?>
@@ -32,30 +32,35 @@
 	}
 	
 	
-	function topLayoutMannagement($pageName,$css,$javaScript) {
+	function topLayoutManagement($pageName,$css,$javaScript) {
 		?>
 		<head>
 			<title><?php echo $pageName; ?></title>
-			<link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-			<link href="css/header.css" rel="stylesheet">
+			<link href="../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+			<link href="../css/headerManagement.css" rel="stylesheet">
 			<?php echo $css; ?>
 			<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-			<script src="css/bootstrap/js/bootstrap.min.js"></script>
-			<script src="js/headerfunctions.js"></script>
-			<script src="js/regex.js"></script>
+			<script src="../css/bootstrap/js/bootstrap.min.js"></script>
+			<script src="../js/headerfunctions.js"></script>
+			<script src="../js/regex.js"></script>
+			<script src="../js/ManagementJS.js"></script>
 			<?php echo $javaScript; ?>
 		</head>
 		<body>
-			<div class="header1 col-xs-12 col-sm-12 col-md-12">
-				<img class="img-responsive logo col-xs-12 col-sm-12 col-md-12" 
-				src="img/logo template.png" alt="logo">
+			<div class="row">
+				<div class="header1 col-xs-12 col-sm-12 col-md-12">
+					<img class="img-responsive logo col-xs-12 col-sm-12 col-md-12"
+					src="../img/logo template.png" alt="logo">
+				</div>
+				<?php include 'admin/menu.php'; ?>
 			</div>
-		
-		
+
+
 		<?php
 	}
-	
+
 	function bottomLayout() {
+
 		echo '</body></html>';
 	}
 ?>
