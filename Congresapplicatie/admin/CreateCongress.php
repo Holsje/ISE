15a1c286208d50f3ab62ef7e4475254bc7a75a27
nielsbrg <nielsbergervoet@gmail.com>
2016-04-21 13:25:31 +0200

    CreateCongress aangepast zodat de + button naast het invoerveld komt. Ook onnodige files van het menu weggegooid.

--- Congresapplicatie/admin/CreateCongress.php
+++ Congresapplicatie/admin/CreateCongress.php
@@ -1,6 +1,7 @@
 <?php
 	require_once('../pageConfig.php');
 	$css = '<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">';
+	$css .= '<link rel="stylesheet" href="../css/createCongress/createCongress.css">';
 	$javaScript = '<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>';
 	$javaScript .= '<script src="../js/globalFunctions.js"></script>';
 	$javaScript .= '<script src="../js/CreateCongress.js"></script>';
@@ -37,8 +38,8 @@
 							</div>
 						</div>
 						<div class="form-group">
-							<label class="control-label col-xs-8 col-sm-4 col-md-4">Subject:</label>
-							<div class="col-xs-12 col-sm-8 col-md-8">
+							<label class="control-label col-xs-4 col-sm-4 col-md-4">Subject:</label>
+							<div class="col-xs-10 col-sm-7 col-md-7 subjectInput">
 								<select id="Subject" name="Subject" class="form-control">
 									<?php 
 										foreach($CongressApplicationDB->getSubjects() as $array) {
@@ -47,8 +48,8 @@
 									?>
 								</select>
 							</div>
-							<div class="col-xs-1 col-sm-1 col-md-1">
-								<button type="button" onclick="$('#addSubject').toggle('fade');">+</button>
+							<div class="col-xs-1 col-sm-1 col-md-1 addSubjectBox">
+								<button id="addSubjectBox" type="button" onclick="$('#addSubject').toggle('fade');">+</button>
 							</div>
 						</div>
 						<div class="form-group">
