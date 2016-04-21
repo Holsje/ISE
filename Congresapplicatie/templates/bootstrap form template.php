ac5fed1b27dd598116ad0c4b1aaac27e1f466c6a
ErikEvers <erikevers1996@gmail.com>
2016-04-21 11:50:11 +0200

    Changed form css

--- Congresapplicatie/templates/bootstrap form template.php
+++ Congresapplicatie/templates/bootstrap form template.php
@@ -37,4 +37,23 @@
 </form>
 <!-- einde form template -->
 
-
+<form class="form-horizontal col-md-offset-1 col-sm-offset-1 col-xs-offset-1 col-xs-8 col-sm-10 col-md-10">
+	<div class="form-group">
+		<label class="control-label col-xs-8 col-sm-4 col-md-4">Gebruikersnaam</label>
+		<div class="col-xs-12 col-sm-8 col-md-8">
+			<input type="text" class="form-control"/>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-xs-8 col-sm-4 col-md-4">Wachtwoord</label>
+		<div class="col-xs-12 col-sm-8 col-md-8">
+			<input type="text" class="form-control"/>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-md-4 col-sm-4 col-xs-4 pull-right ">
+			<button type="submit" class="btn btn-default form-control">Inloggen</button>
+		</div>
+	</div>
+</form>
+<!-- template erik -->
