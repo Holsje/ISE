31a5a0d185eef907efea7c312c713489442f068c
nielsbrg <nielsbergervoet@gmail.com>
2016-04-21 10:13:50 +0200

    Bootstrap form template

---
+++ Congresapplicatie/templates/bootstrap form template.php
@@ -0,0 +1,40 @@
+<form class="form-horizontal col-xs-8 col-sm-6 col-md-6">
+	<div class="form-group">
+		<label class="control-label col-xs-5 col-sm-4 col-md-3">Congresnaam</label>
+		<div class="col-xs-7 col-sm-8 col-md-9">
+			<input type="text" class="form-control"/>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-xs-5 col-sm-4 col-md-3">Plaats</label>
+		<div class="col-xs-7 col-sm-8 col-md-9">
+			<input type="text" class="form-control"/>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-xs-5 col-sm-4 col-md-3">Onderwerpen</label>
+		<div class="col-xs-7 col-sm-8 col-md-9">
+			<select class="form-control">
+				<option>Data Modeling</option>
+				<option>Data Implementation</option>
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-xs-5 col-sm-4 col-md-3">Onderwerpen</label>
+		<div class="col-xs-7 col-sm-8 col-md-9">
+			<select multiple class="form-control">
+				<option>Data Modeling</option>
+				<option>Data Implementation</option>
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-md-4 pull-right">
+			<button type="submit" class="btn btn-default form-control">Submit form</button>
+		</div>
+	</div>
+</form>
+<!-- einde form template -->
+
+
