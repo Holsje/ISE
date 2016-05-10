1adf4b1fba1f1c104845c729fff888e59a57d16e
ErikEvers <erikevers1996@gmail.com>
2016-05-10 11:33:09 +0200

    Fixed login liberties

--- Congresapplicatie/admin/InlogSubmit.php
+++ Congresapplicatie/admin/InlogSubmit.php
@@ -5,21 +5,23 @@
  * Date: 9-5-2016
  * Time: 13:00
  */
-
+    $errorstring = "";
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         if (isset($_POST['login'])){
             if ($login->checkLogin($_POST['input-username'], $_POST['input-password'])){
                 //setcookie('user', $_POST['input-username'], time() + (14*24*60*60));
                 $_SESSION['user'] = $_POST['input-username'];
                 echo "Ingevulde inloggegevens zijn juist!";
+                $_SESSION['liberties'] = $login->checkUser($_SESSION['user']);
                 header('Location: index.php');
             }
             else{
-                echo "Ingevulde inloggegevens zijn onjuist!";
+                $errorstring = "Ingevulde inloggegevens zijn onjuist!";
             }
         }
         else if (isset($_POST['logout'])){
             unset($_SESSION['user']);
+            unset($_SESSION['liberties']);
             if(isset($_COOKIE['user'])) {
                 setcookie('user', '', 1);
             }
