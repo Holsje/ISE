a9dfc2c07466b51f08cafbd434dfd920d04359ed
Holsje <onnoajax@gmail.com>
2016-05-11 11:47:02 +0200

    Update ManageCongress.php

--- Congresapplicatie/admin/ManageCongress.php
+++ Congresapplicatie/admin/ManageCongress.php
@@ -13,20 +13,20 @@ $js = '<script type="text/javascript" src="../js/congressManagement.js"></script
 topLayoutManagement('Beheren Congres',null,$js);
 ?>
 
-    <div class="row">
-        <div class="container   col-md-12 col-xs-12">
-            <div class="content col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2">
-                <?php
+<div class="row">
+    <div class="container   col-md-12 col-xs-12">
+        <div class="content col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2">
+            <?php
                 $manageCongress->createManagementScreen(array("Congresnummer", "Naam", "Onderwerp", "Locatie", "Startdatum", "Einddatum"), $manageCongress->getCongresses());
             ?>
-            </div>
         </div>
     </div>
+</div>
 
-    <?php 
-$manageCongress->createCreateCongressScreen(); 
-$manageCongress->createEditCongressScreen(); 
-
-bottomLayout();
+<?php 
+    $manageCongress->createCreateCongressScreen(); 
+    $manageCongress->createEditCongressScreen(); 
+    
+    bottomLayout();
 
 ?>
