12c720fa3d1fa07ed4f8638679849c68b25a0708
Daniel de Jong <danieldejongg@hotmail.com>
2016-05-20 14:04:59 +0200

    Database connectie in combinatie met evenementen klaar.

--- Congresapplicatie/Index_Class.php
+++ Congresapplicatie/Index_Class.php
@@ -22,26 +22,50 @@
         }
         
         public function getEventInfo($eventNo,$congresNo){
-            $sqlStatement = 'SELECT ENAME, SUBJECT, FILEDIRECTORY
+            $sqlStatement = 'SELECT ENAME, SUBJECT, FILEDIRECTORY, DESCRIPTION
                              FROM EVENT
                              WHERE EVENTNO = ? AND CONGRESSNO = ?';
+            $sqlStatementSpeakers = 'SELECT P.PERSONNO, S.PICTUREPATH, P.FIRSTNAME, P.LASTNAME
+                                     FROM SPEAKEROFEVENT SE INNER JOIN SPEAKER S 
+                                         ON SE.PERSONNO = S.PERSONNO INNER JOIN PERSON P 
+                                             ON P.PERSONNO = S.PERSONNO
+                                     WHERE SE.EVENTNO = ? AND SE.CONGRESSNO = ?';
+            $paramsSpeakers = array($eventNo,$congresNo);
+            $resultSpeakers = $this->database->sendQuery($sqlStatementSpeakers, $paramsSpeakers);
+            $arraySpeakers = array();
+            if($resultSpeakers){
+                while($row = sqlsrv_fetch_array($resultSpeakers,SQLSRV_FETCH_ASSOC)){
+                    array_push($arraySpeakers,$row);
+                }
+            }
             $params = array($eventNo,$congresNo);
             $result = $this->database->sendQuery($sqlStatement,$params);
             if($result){
                 if($row = sqlsrv_fetch_array($result,SQLSRV_FETCH_ASSOC)){
-                    return json_encode($row);
+                    $row['speakers'] = $arraySpeakers;
+                    return json_encode($row, JSON_FORCE_OBJECT);
                 }
             }
         }
         
         public function createCongresOverzicht(){
+            $sqlStatement = 'SELECT EVENTNO, ENAME, FILEDIRECTORY, DESCRIPTION
+                             FROM EVENT
+                             WHERE CONGRESSNO = ?';
+            $params = array($_SESSION['congresNo']);
+            $result = $this->database->sendQuery($sqlStatement,$params);
+            if($result){
+                while($row = sqlsrv_fetch_array($result,SQLSRV_FETCH_ASSOC)){
+                    $this->createScreen->createEventInfo($row['ENAME'],$row['DESCRIPTION'],$row['EVENTNO'],'#popUpeventInfo','col-sm-3 col-md-3 col-xs-3','margin-right:50px; margin-bottom:50px; ',$row['FILEDIRECTORY'] . 'thumbnail.png','');
+                }
+            }
             //($eventName,$description,$eventId,$dataFile,$classes,$extraStyle,$image,$timeString)
-            $this->createScreen->createEventInfo('TestEvent1','Het is al geruime tijd een bekend gegeven dat een lezer, tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. ...','1','#popUpeventInfo','col-sm-3 col-md-3 col-xs-3','margin-right:50px; margin-bottom:50px; ','img/thumbnail.png','12:00 - 15:00');
+            
         }
         
         public function createEventInfoPopup(){
-            $image = new Img('','','thumbnail','col-md-4 col-sm-6',true,false);
-            $spanDescription = new Span('','','eventDescription','col-md-8 col-sm-6',false,true);
+            $image = new Img('','','thumbnail','col-md-3 col-sm-4',true,false);
+            $spanDescription = new Span('','Over evenement','eventDescription','col-md-8 col-sm-6',false,true);
             $spanSubjects = new Span('','Onderwerp(en)','subjects','col-md-12 col-sm-12',true,true);
             $spanSpeakers = new Span('','Spreker(s)','speakers','',true,true);
             $this->createScreen->createPopup(array($image,$spanDescription,$spanSubjects,$spanSpeakers),"","eventInfo",null);
--- Congresapplicatie/js/public.js
+++ Congresapplicatie/js/public.js
@@ -1,9 +1,10 @@
-var target;
-
 $(document).ready(function () {
     $(".moreInfoButton").on("click", function (event) {
-        target = $('.popupTitle h1');
         getEventInfo($(this).closest("div").attr("value"));
+    });
+
+    $("#popUpeventInfo .closePopup").on("click", function (event) {
+        $('.speaker').remove();
     });
 });
 
@@ -21,6 +22,36 @@ function getEventInfo(eventNo) {
             $('.popupTitle h1').html(data['ENAME']);
             $('#thumbnail').attr('src', data['FILEDIRECTORY'] + 'thumbnail.png');
             $('#subjects').html(data['SUBJECT']);
+            $('#eventDescription').html(data['DESCRIPTION']);
+            var sizeSpeaker = 0;
+            for (var value in data['speakers']) {
+                sizeSpeaker++;
+            }
+
+            for (i = 0; i < sizeSpeaker; i++) {
+                console.log(data['speakers'][i]['FIRSTNAME']);
+                var formGroup = document.createElement('DIV');
+                formGroup.className += 'form-group';
+                formGroup.className += ' speaker';
+                formGroup.className += ' col-md-2';
+                var img = document.createElement('IMG');
+                img.className += 'col-md-12';
+                img.src = data['speakers'][i]['PICTUREPATH'];
+                formGroup.appendChild(img);
+                var name = document.createElement('SPAN');
+                name.innerHTML = data['speakers'][i]['FIRSTNAME'] + ' ' + data['speakers'][i]['LASTNAME'];
+                name.className += 'col-md-10';
+                formGroup.appendChild(name);
+                document.forms['formeventInfo'].appendChild(formGroup);
+            }
+
+        },
+        error: function (request, status, error) {
+            alert(request.responseText);
         }
     });
+}
+
+function createSpeakerElement(index) {
+    console.log(index);
 }
