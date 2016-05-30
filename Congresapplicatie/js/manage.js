19e801f25287eb98660a6521dac698b612c5164a
nielsbrg <nielsbergervoet@gmail.com>
2016-05-30 13:55:06 +0200

    Manage css en js

--- Congresapplicatie/js/manage.js
+++ Congresapplicatie/js/manage.js
@@ -1,4 +1,35 @@
 $(document).ready(function () {
+	$(".dataTables_scrollBody").removeAttr("style");
+	$(".dataTables_scrollBody").addClass("scrollBody");
 	
-	alert('javascript aangeroepen');
+	$(".locationSelect").change(function() {
+		var selectedValue = $(this).val();
+		alert(getLocationName(selectedValue));
+		$.ajax({
+		url: window.location.href,
+        type: 'POST',
+        data: {
+			LocationName: getLocationName(selectedValue),
+			City: getLocationCity(selectedValue)
+        },
+		success: function(data) {
+			console.log(data);
+		}});
+	})
 });
+
+function getLocationName(value) {
+	locationArray = value.split(" ");
+	locationArray.pop();
+	locationArray.pop();
+	returnString = '';
+	for(var i = 0; i < locationArray.length; i++) {
+		returnString += ' ' + locationArray[i]
+	}
+	return returnString;
+}
+
+function getLocationCity(value) {
+	locationArray = value.split(" ");
+	return locationArray.last();
+}
