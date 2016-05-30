9ddcd58112f3a6894001e6e5ae58e3ec02e63b11
Onno <o.hols@han.nl>
2016-05-30 20:45:38 +0200

    no message

--- Congresapplicatie/js/DataSwap.js
+++ Congresapplicatie/js/DataSwap.js
@@ -32,9 +32,8 @@ $(document).ready(function () {
 
 
 function goRight(event) {
-	console.log(event.target.attributes.getNamedItem("data-file").value + "left");
 	var selectedRows = dataSwapTables[event.target.attributes.getNamedItem("data-file").value].rows(".selected");
-	if(keepRight == true) {
+	if(event.target.attributes.getNamedItem("remove").value == true) {
 		selectedRows.remove().draw(false);
 	}else {
 		for(var i = 0;i<selectedRows.data().length;i++) {
