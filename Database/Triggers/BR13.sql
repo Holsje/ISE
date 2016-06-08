31b288d941afb389bea3bc07f5673df7cff21258
Daniel de Jong <danieldejongg@hotmail.com>
2016-06-08 16:16:37 +0200

    UpdateBR13

--- Database/Triggers/BR13.sql
+++ Database/Triggers/BR13.sql
@@ -35,3 +35,80 @@ BEGIN TRAN
 ROLLBACK TRAN
 
 SELECT * FROM SPEAKER
+GO
+CREATE TRIGGER trRemovePersonTypeOfPersonOnVisitor
+  ON Visitor
+  AFTER DELETE
+  AS 
+  BEGIN
+	IF @@ROWCOUNT = 0 RETURN;
+	SET NOCOUNT ON;
+	BEGIN TRY
+		DELETE PTOP 
+		FROM PersonTypeOfPerson PTOP
+		INNER JOIN deleted D ON D.PersonNo = PTOP.PersonNo
+		WHERE PTOP.TypeName = 'Bezoeker'
+	END TRY
+	BEGIN CATCH
+		THROW;
+	END CATCH
+  END
+GO
+CREATE TRIGGER trRemovePersonTypeOfPersonOnCongressMananger
+ON	CongressManager
+AFTER DELETE
+AS 
+BEGIN
+IF @@ROWCOUNT = 0 RETURN;
+SET NOCOUNT ON;
+BEGIN TRY
+	DELETE PTOP 
+	FROM PersonTypeOfPerson PTOP
+	INNER JOIN deleted D ON D.PersonNo = PTOP.PersonNo
+	WHERE PTOP.TypeName = 'Congresbeheerder'
+END TRY
+BEGIN CATCH
+	THROW;
+END CATCH
+END
+GO
+CREATE TRIGGER trRemovePersonTypeOfPersonOnGeneralManager
+ON	GeneralManager
+AFTER DELETE
+AS 
+BEGIN
+IF @@ROWCOUNT = 0 RETURN;
+SET NOCOUNT ON;
+BEGIN TRY
+	DELETE PTOP 
+	FROM PersonTypeOfPerson PTOP
+	INNER JOIN deleted D ON D.PersonNo = PTOP.PersonNo
+	WHERE PTOP.TypeName = 'Algemene beheerder'
+END TRY
+BEGIN CATCH
+	THROW;
+END CATCH
+END
+
+GO
+CREATE TRIGGER trRemovePersonTypeOfPersonOnReviewBoard
+ON	ReviewBoard
+AFTER DELETE
+AS 
+BEGIN
+IF @@ROWCOUNT = 0 RETURN;
+SET NOCOUNT ON;
+BEGIN TRY
+	DELETE PTOP 
+	FROM PersonTypeOfPerson PTOP
+	INNER JOIN deleted D ON D.PersonNo = PTOP.PersonNo
+	WHERE PTOP.TypeName = 'Reviewboard'
+END TRY
+BEGIN CATCH
+	THROW;
+END CATCH
+END
+
+
+
+SELECT * FROM PersonType
